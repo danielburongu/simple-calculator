ea411d972b3aec7fd6ba5e9e4e135af68b81c477
daniel burongu <burongudaniel@gmail.com>
2022-10-30 00:49:14 +0200

    how to delete items from lists

---
+++ remove.py
@@ -0,0 +1,9 @@
+#how to remove items from lists
+#del command helps us remove an item
+#when working with lists use square brackets
+
+my_names=["daniel","joel","brandy","travis"]
+#with lists we start counting from zero
+del my_names[0]
+print(my_names)
+#the above code will remove daniel because it occupies space 0
--- sorting.py
+++ sorting.py
@@ -1,4 +1,4 @@
-#sorting/organising items in order 
+#sorting/organising items in order(lists) 
 #we can be able to sort out lists
 my_list=['d','q','r','e','t']
 #This will organise the above letters in alphabetical order
