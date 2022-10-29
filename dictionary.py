5ea6f57ffdf6a654a51b9a3f078cdf94fba004ed
daniel burongu <burongudaniel@gmail.com>
2022-10-30 00:20:28 +0200

    added a dictionary file

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#creat a dictionary to store information about about a person
+person={
+    "name":"daniel",
+    "age":22,
+    "country":"Uganda"
+}
+print(person["name"])
+print(person["age"])
+print(person["country"])
